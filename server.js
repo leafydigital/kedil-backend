6a53ea2c6e0df92f9e458693c978864bbcbd085b
Ram <leafydigital@gmail.com>
2025-04-23 13:44:04 +0530

    Backend Commit

--- server.js
+++ server.js
@@ -57,4 +57,6 @@ app.use("/api/transaction", require("./routes/transactionsRoutes"));
 
 app.use("/api/budget", require("./routes/budgetRoutes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+module.exports = app;
+
+//app.listen(5000, () => console.log("Server running on port 5000"));
